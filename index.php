af3e53e72fd06d040156f63b3ceb9ebe9b2a109f
Maxime Badin <maxbadin38@gmail.com>
2024-04-17 20:21:28 +0200

    Fix: Indentation

--- index.php
+++ index.php
@@ -1,23 +1,23 @@
 <!doctype html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link
-        rel="stylesheet"
-        href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css"
-    />
-    <title>PHP - WHOAMI</title>
-</head>
-<body>
-    <div class="container" style="text-align: center; margin-top: 5vh">
-        <h1>PHP - WHOAMI</h1>
-        <p>
-            <?php
-                echo "Hello, I'm a PHP script running on " . gethostname();
-            ?>
-        </p>
-    </div>
-</body>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link
+            rel="stylesheet"
+            href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css"
+        />
+        <title>PHP - WHOAMI</title>
+    </head>
+    <body>
+        <div class="container" style="text-align: center; margin-top: 5vh">
+            <h1>PHP - WHOAMI</h1>
+            <p>
+                <?php
+                    echo "Hello, I'm a PHP script running on " . gethostname();
+                ?>
+            </p>
+        </div>
+    </body>
 </html>
